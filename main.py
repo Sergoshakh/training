0d60cf450b298f1803878ab9584386fad84759c6
Sergoshakh <sergo.shakh@gmail.com>
2024-09-20 20:51:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("1st program")
+1st program
+>>> print(9**0.5*5)
+15.0
+>>> print("2nd program")
+2nd program
+>>> print((9.99 > 9.98) and (1000 != 1000.1))
+True
+>>> print("3rd program")
+3rd program
+>>> print(2*2+2)
+6
+>>> print(2*(2+2))
+8
+>>> print((2*2+2) == (2*(2+2)))
+False
+>>> print("4th program")
+4th program
+>>> print(float("123.456"))
+123.456
+>>> print(float("123.456")*10)
+1234.56
+>>> print(int(float("123.456")*10))
+1234
+>>> print(int(float("123.456")*10)%10)
+4
+>>> print("end of lesson")
+end of lesson
+>>> 
